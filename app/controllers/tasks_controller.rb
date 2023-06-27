9b4a75af36e198900df267438851fa2b7adb6177
dumbled0re <smu315064@gmail.com>
2023-06-27 16:30:06 +0900

    feat: コントローラーの追加

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,28 @@
+class TasksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    # flash[:notice] = "タスク「#{task.name}」を登録しました。"
+    # redirect_to tasks_url
+    # 上記のコードを下記のように書き換えることができる
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+end
