57e5a1c0d1608ee039af2f61d870896b4f760b57
dumbled0re <smu315064@gmail.com>
2023-07-02 00:52:03 +0900

    fix: 管理者のみが管理者機能を使用できるように修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < ApplicationController
+  before_action :require_admin
+
   def index
     @users = User.all
   end
@@ -42,7 +44,12 @@ class Admin::UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def require_admin
+    redirect_to root_url unless current_user.admin?
   end
 end
