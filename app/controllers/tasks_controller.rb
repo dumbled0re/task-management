58060e4218e82966db5ca396ac924e2520b4b8cf
dumbled0re <smu315064@gmail.com>
2023-07-02 00:42:59 +0900

    update: 特定のユーザのタスクのみを操作できるように修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
   def index
     # この時点ではまだ、データベースからデータを取得していない
-    @tasks = Task.all
+    @tasks = current_user.tasks
+    # @tasks = Task.where(user_id: current_user.id)
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -13,26 +15,42 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    # flash[:notice] = "タスク「#{task.name}」を登録しました。"
-    # redirect_to tasks_url
-    # 上記のコードを下記のように書き換えることができる
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    @task = current_user.tasks.new(task_params)
+
+    if @task.save
+      # flash[:notice] = "タスク「#{task.name}」を登録しました。"
+      # redirect_to tasks_url
+      # 上記のコードを下記のように書き換えることができる
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      # エラーの場合は、newアクションを実行する
+      render :new
+    end
+
+    # @task = current_user.tasks.new(task_params)
+
+    # if @task.save
+    #   redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    # else
+    #   render :new
+    # end
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id]) # 既存のタスクを取得
+    # task = Task.find(params[:id]) # 既存のタスクを取得
+    task = current_user.tasks.find(params[:id])
     task.update!(task_params) # 既存のタスクの属性を更新
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
   def destroy
-    task = Task.find(params[:id]) # 既存のタスクを取得
+    # task = Task.find(params[:id]) # 既存のタスクを取得
+    task = current_user.tasks.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
