3a8d11b41a9b444533b154eac6f3275b7c487dbf
dumbled0re <smu315064@gmail.com>
2023-07-02 00:33:25 +0900

    feat: ログインしているユーザー情報を取得する関数とログインしていない場合は、ログイン画面にリダイレクトする関数を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  before_action :login_required
+
+  private
+
+  # ログインしているユーザーを取得する
+  def current_user
+    # session[:user_id]がnilの場合は、nilを返す
+    # find_byは、データが見つからなかった場合にnilを返す
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  # ログインしていない場合は、ログイン画面にリダイレクトする
+  def login_required
+    redirect_to login_path unless current_user
+  end
 end
