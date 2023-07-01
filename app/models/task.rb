173ced240be10a0b72c34435b616466d84b444cb
dumbled0re <smu315064@gmail.com>
2023-07-01 10:19:42 +0900

    feat: name属性にカンマが入らないように検証を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,19 @@
 class Task < ApplicationRecord
+  # name属性に値が設定されていない場合には、データベースへの保存ができなくなります(検証)
+  validates :name, presence: true, length: { maximum: 30 }
+  # 自前の検証メソッドを定義する
+  validate :validate_name_not_include_comma
+  # 検証前に実行されるコールバックメソッド(アプリでは使用しないのでコメントアウト)
+  # before_validation :set_nameless_name
+
+  private
+
+  # def set_nameless_name
+  #   self.name = '名前なし' if name.blank?
+  # end
+
+  def validate_name_not_include_comma
+    # name属性にカンマが含まれている場合には、エラーを追加する(&.でnilの場合には処理をスキップするようにしているがDBでnull制約をしているので検証エラーが発生する)
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
